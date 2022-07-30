154f4710df1d7bd876dae6ae65ea0100c35e8f36
bec <becsun@icloud.com>
2022-07-30 17:23:53 +0100

    spring boot added

---
+++ wordOfTheDay/src/test/java/com/one/wordOfTheDay/WordOfTheDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one.wordOfTheDay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordOfTheDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
